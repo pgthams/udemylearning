24c588e65593609cc0ba9c487f12bfbaebbd14a5
tparasur <tparasur@starbucks.com>
2022-09-20 12:29:08 -0700

    Firstcommit

--- UdemyLearning/ViewController.swift
+++ UdemyLearning/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var flowerLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func changeButton(_ sender: Any) {
+        imageView.image = UIImage(named: "img4")
+        
+    }
+    
 }
 
